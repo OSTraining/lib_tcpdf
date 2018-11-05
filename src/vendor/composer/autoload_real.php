489963aef02c527789c47e491f49c2bf141ba671
Bill Tomczak <bill@ostraining.com>
2018-11-05 09:10:03 -0800

    Update to more recent version of TCPDF

--- src/vendor/composer/autoload_real.php
+++ src/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit7ace22756ffd43c197bb8765abb0962f
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit7ace22756ffd43c197bb8765abb0962f', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
